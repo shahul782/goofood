1b6c0f25bc4ebe762ff63c356040c5f9a7f38597
shahul782 <shahul1145@gmail.com>
2024-02-18 11:58:17 -0800

    change-placeholder

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,7 +6,7 @@ export const Carousel = () => {
     <div className="carousel-inner" id='carousel'>
       <div className='carousel-caption' style={{zIndex:"10"}}>
       <form className="d-flex">
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <input className="form-control me-2" type="search" placeholder="SEARCH" aria-label="Search"/>
       <button className="btn btn-outline-success text-white bg-success" type="submit">Search</button>
     </form>
       </div>
